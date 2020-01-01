0aecb44f2fa69fbb9502bf32db55e130721938ad
Bryle <brylemp@yahoo.com>
2020-01-01 22:30:39 +0800

    Bsta kana

--- mainUI.py
+++ mainUI.py
@@ -1,39 +1,68 @@
-from Tkinter import *
+from tkinter import *
+from historyUI import *
 from PIL import ImageTk, Image
 
-window = Tk()
-window.title("weweew") 
-window.geometry("1000x600") #Size for Window
-# window.overrideredirect(1) #Remove window border
-window.resizable(False,False) #Prevent resize windows
+def sync():
+    print("Sync!")
 
-####### BG through Pillow PIL ########
-C = Canvas(window, bg="black", height=100, width=300) 
-bg_image = ImageTk.PhotoImage(Image.open("db.png")) # BG through Pillow PIL
-background_label = Label(window, image=bg_image) 
-background_label.place(x=0, y=0, relwidth=1, relheight=1) 
-C.pack()
+def showhide():
+    print("Hidden")
 
-###### BUTTON IMAGES LOAD #######
-sd_image = ImageTk.PhotoImage(Image.open("sd.png"))
-sy_image = ImageTk.PhotoImage(Image.open("sync.png"))
-showhide_image = ImageTk.PhotoImage(Image.open("showhide.png"))
-hist_image = ImageTk.PhotoImage(Image.open("hist.png"))
+def historyUI(window):
+    print("History!")
+    ##### FRAME ######
+    history_frame = Frame(window)
+    history_frame.pack(expand=1, fill=BOTH)
+    history_frame.lift()
 
-#### SYNC ####
-syB = Button (window, image=sy_image, width=182, height=74, border=0, highlightthickness=0, highlightbackground="#ffffff", command=window.destroy)
-syB.place(bordermode=OUTSIDE,x=550,y=349)
+    ####### BG through Pillow PIL ########
+    BackGround = Canvas(history_frame, bg="black", height=1000, width=600) 
+    bg_image = ImageTk.PhotoImage(Image.open("db.png")) # BG through Pillow PIL
+    background_label = Label(history_frame, image=bg_image) 
+    background_label.place(x=0, y=0, relwidth=1, relheight=1) 
+    BackGround.pack()
 
-#### SHOW/HIDE ####
-showhideB = Button (window, image=showhide_image, width=182, height=74, border=0, highlightthickness=0, highlightbackground="#ffffff", command=window.destroy)
-showhideB.place(bordermode=OUTSIDE,x=760,y=349)
+def mainUI():
+    ##### FRAME ######
+    main_frame = Frame(window)
+    main_frame.pack(expand=1, fill=BOTH, side="top")
 
-#### HISTORY ####
-histB = Button (window, image=hist_image, width=182, height=74, border=0, highlightthickness=0, highlightbackground="#ffffff", command=window.destroy)
-histB.place(bordermode=OUTSIDE,x=550,y=459)
+    ####### BG through Pillow PIL ########
+    BackGround = Canvas(main_frame, bg="black", height=1000, width=600) 
+    bg_image = ImageTk.PhotoImage(Image.open("qw.png")) # BG through Pillow PIL
+    background_label = Label(main_frame, image=bg_image) 
+    background_label.place(x=0, y=0, relwidth=1, relheight=1) 
+    BackGround.pack()
 
-#### SHUTDOWN ####
-shutdownB = Button (window, image=sd_image, width=182, height=74, border=0, highlightthickness=0, highlightbackground="#ffffff", command=window.destroy) 
-shutdownB.place(bordermode=OUTSIDE,x=760,y=459)
+    ###### BUTTON IMAGES LOAD #######
+    sd_image = ImageTk.PhotoImage(Image.open("sd.png"))
+    sy_image = ImageTk.PhotoImage(Image.open("sync.png"))
+    showhide_image = ImageTk.PhotoImage(Image.open("showhide.png"))
+    hist_image = ImageTk.PhotoImage(Image.open("hist.png"))
 
-window.mainloop() #Start
+    #### SYNC ####
+    syB = Button (main_frame, image=sy_image, width=182, height=74, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=sync)
+    syB.place(bordermode=OUTSIDE,x=550,y=349)
+
+    #### SHOW/HIDE ####
+    showhideB = Button (main_frame, image=showhide_image, width=182, height=74, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=showhide)
+    showhideB.place(bordermode=OUTSIDE,x=762,y=349)
+
+    #### HISTORY ####
+    histB = Button (main_frame, image=hist_image, width=182, height=74, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: historyUI(window))
+    histB.place(bordermode=OUTSIDE,x=550,y=460)
+
+    #### SHUTDOWN ####
+    shutdownB = Button (main_frame, image=sd_image, width=182, height=74, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=window.destroy) 
+    shutdownB.place(bordermode=OUTSIDE,x=760,y=459)
+
+    window.mainloop() #Start
+
+
+if __name__ == "__main__":
+    ##### WINDOW #####
+    window = Tk()
+    window.geometry("1000x600") #Size for Window
+    # window.overrideredirect(1) #Remove window border
+    window.resizable(False,False) #Prevent resize windows
+    mainUI()
