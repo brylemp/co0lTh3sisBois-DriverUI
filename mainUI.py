a040635f31c25ce28b06d1c8d00bd7d7fa13f443
Bryle Mac <brylemp@yahoo.com>
2020-01-02 23:17:10 +0800

    Added Hidden and Shown Feature(Global)

--- mainUI.py
+++ mainUI.py
@@ -2,12 +2,24 @@ from tkinter import *
 from historyUI import *
 from PIL import ImageTk, Image
 
+shflag=0
+
 def sync():
     print("Sync!")
 
-def showhide():
-    print("Hidden")
-
+def showhide(main_totalfare,main_totalpass):
+    global shflag
+    if shflag == 0:
+        print("Hidden!")
+        main_totalfare.config(text="Hidden")
+        main_totalpass.config(text="Hidden")
+        shflag = 1
+    elif shflag == 1:
+        print("Shown!")
+        main_totalfare.config(text="₱500")
+        main_totalpass.config(text="100")
+        shflag = 0
+        
 def mainUI(window):
     ##### FRAME ######
     main_frame = Frame(window)
@@ -17,14 +29,21 @@ def mainUI(window):
     hist_frame.pack_forget()
     
     ####### MAIN UI BG through Pillow PIL ########
-    BackGround = Canvas(main_frame, bg="black", height=1000, width=600) 
-    bg_image = ImageTk.PhotoImage(Image.open("db.png")) # BG through Pillow PIL
-    background_label = Label(main_frame, image=bg_image) 
-    background_label.place(x=0, y=0, relwidth=1, relheight=1) 
-    BackGround.pack()
+    main_bg = Canvas(main_frame, bg="#e3e3e3", height=1000, width=600) 
+    main_bg_image = ImageTk.PhotoImage(Image.open("newdb.png")) # BG through Pillow PIL
+    main_label = Label(main_frame, image=main_bg_image) 
+    main_label.place(x=0, y=0, relwidth=1, relheight=1) 
+    main_bg.pack()
+
+    ###### MAIN UI LABELS ###########
+    main_totalfare = Label(main_frame, width="7", bd=0, bg="#e3e3e3", fg="#00ad31", font=("Arial",75), text="₱500")
+    main_totalfare.place(x=105,y=72)
+
+    main_totalpass = Label(main_frame, width="7", bd=0, bg="#e3e3e3", fg="#00ad31", font=("Arial",75), text=shflag)
+    main_totalpass.place(x=105,y=372)
 
     ####### HISTORY UI BG through Pillow PIL ########
-    hist_bg = Canvas(hist_frame, bg="black", height=1000, width=600) 
+    hist_bg = Canvas(hist_frame, bg="#e3e3e3", height=1000, width=600) 
     hist_bg_image = ImageTk.PhotoImage(Image.open("hist_screen.png")) # BG through Pillow PIL
     hist_label = Label(hist_frame, image=hist_bg_image) 
     hist_label.place(x=0, y=0, relwidth=1, relheight=1) 
@@ -42,7 +61,7 @@ def mainUI(window):
     syB.place(bordermode=OUTSIDE,x=550,y=349)
 
     #### SHOW/HIDE ####
-    showhideB = Button (main_frame, image=showhide_image, width=182, height=74, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=showhide)
+    showhideB = Button (main_frame, image=showhide_image, width=182, height=74, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: showhide(main_totalfare,main_totalpass))
     showhideB.place(bordermode=OUTSIDE,x=762,y=349)
 
     #### HISTORY ####
@@ -53,7 +72,7 @@ def mainUI(window):
     shutdownB = Button (main_frame, image=sd_image, width=182, height=74, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=window.destroy) 
     shutdownB.place(bordermode=OUTSIDE,x=760,y=459)
 
-    ###### BUTTONS FOR MAIN UI #######
+    ###### BUTTONS FOR HISTORY UI #######
     ###### BUTTON IMAGES LOAD #######
     bk = ImageTk.PhotoImage(Image.open("back.png"))
     nx = ImageTk.PhotoImage(Image.open("next.png"))
@@ -72,7 +91,6 @@ def mainUI(window):
     prevB.place(bordermode=OUTSIDE,x=275,y=510)
 
     window.mainloop() #Start
-
 
 if __name__ == "__main__":
     ##### WINDOW #####
