feedb2a59d9705008353cc3cf1d6ba5484c58b5f
Bryle Mac <brylemp@yahoo.com>
2020-01-27 19:21:03 +0800

    Removed mainUI, Wifi status changes (Patalinghug1 == connected, Patalinghug2 == not connected)

--- mainUI.py
+++ mainUI.py
@@ -9,22 +9,17 @@ Total_Passenger = 300
 Driver_Name = "Dela Cruz, Juan Paolo"
 Wifi_Status = 0
 
-def is_wifi(window,wifi_label,nowifi_label):
-    global Driver_Name
+def is_wifi():
     wat = os.popen('iwgetid').read()
     watt = re.findall('"([^"]*)"',wat) ##FIND ENCLOSED IN ""##
     watt = ''.join(watt) ##CONVERT LIST TO STRING##
-    if watt == "Patalinghug2":
-        print(watt)
-        Driver_Name = "Pat2"
-        nowifi_label.place(x=10,y=10)
-        wifi_label.place_forget
+    if watt == "Patalinghug1":
+        replace = ImageTk.PhotoImage(Image.open("wifistatus.png"))
     else:
-        print(watt)
-        Driver_Name = "Pat1"
-        wifi_label.place(x=10,y=10)
-        nowifi_label.place_forget
+        replace = ImageTk.PhotoImage(Image.open("nowifi.png"))
         
+    wifi_label.config(image=replace)
+    wifi_label.image=replace
     window.after(5000, is_wifi)
 
 def sync(wifi_label,nowifi_label):
@@ -56,98 +51,94 @@ def showhide(main_totalfare,main_totalpass):
         main_totalpass.config(text=Total_Passenger)
         shflag = 0
         
-def mainUI(window):
-    ##### FRAME ######
-    main_frame = Frame(window)
-    main_frame.pack(expand=1,fill=BOTH)
 
-    hist_frame = Frame(window)
-    hist_frame.pack_forget()
+##### WINDOW #####
+window = Tk()
+window.geometry("848x480") #Size for Window
+# window.overrideredirect(1) #Remove window border
+window.resizable(False,False) #Prevent resize windows
+
+##### FRAME ######
+main_frame = Frame(window)
+main_frame.pack(expand=1,fill=BOTH)
+
+hist_frame = Frame(window)
+hist_frame.pack_forget()
+
+####### MAIN UI BG through Pillow PIL ########
+main_bg = Canvas(main_frame, bg="#e3e3e3", height=480, width=848) 
+main_bg_image = ImageTk.PhotoImage(Image.open("emptybg.png")) # BG through Pillow PIL
+main_label = Label(main_frame, image=main_bg_image) 
+main_label.place(x=0, y=0, relwidth=1, relheight=1) 
+main_bg.pack()
+
+###### MAIN UI LABELS ###########
+TTF = "₱"+str(Total_Fare)
+main_totalfare = Label(main_frame, width="7", bd=0, bg="#e3e3e3", fg="#00ad31", font=("ArialUnicodeMS",55), text=TTF)
+main_totalfare.place(x=70,y=32)
+
+main_totalpass = Label(main_frame, width="7", bd=0, bg="#e3e3e3", fg="#00ad31", font=("ArialUnicodeMS",55), text=Total_Passenger)
+main_totalpass.place(x=70,y=282)
+
+main_drivername = Label(main_frame, anchor="sw", width="25", bd=0, bg="#e3e3e3", fg="#000000", font=("ArialUnicodeMS",15), text=Driver_Name)
+main_drivername.place(x=10,y=445) 
+
+wifi_image = ImageTk.PhotoImage(Image.open("wifistatus.png"))
+wifi_label = Label(main_frame, image=wifi_image, bd=0, bg="#e3e3e3") 
+wifi_label.place(x=10,y=10)
+
+
+####### HISTORY UI BG through Pillow PIL ########
+hist_bg = Canvas(hist_frame, bg="#e3e3e3", height=480, width=848) 
+hist_bg_image = ImageTk.PhotoImage(Image.open("hist_screen.png")) # BG through Pillow PIL
+hist_label = Label(hist_frame, image=hist_bg_image) 
+hist_label.place(x=0, y=0, relwidth=1, relheight=1) 
+hist_bg.pack()
+
+###### BUTTONS FOR MAIN UI #######
+###### BUTTON IMAGES LOAD #######
+sd_image = ImageTk.PhotoImage(Image.open("sd.png"))
+sy_image = ImageTk.PhotoImage(Image.open("sync.png"))
+showhide_image = ImageTk.PhotoImage(Image.open("showhide.png"))
+hist_image = ImageTk.PhotoImage(Image.open("hist.png"))
+
+#### SYNC ####
+syB = Button (main_frame, image=sy_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: sync(wifi_label,nowifi_label))
+syB.place(bordermode=OUTSIDE,x=438,y=258)
+
+#### SHOW/HIDE ####
+showhideB = Button (main_frame, image=showhide_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: showhide(main_totalfare,main_totalpass))
+showhideB.place(bordermode=OUTSIDE,x=650,y=258)
+
+#### HISTORY ####
+histB = Button (main_frame, image=hist_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack_forget(),hist_frame.pack(expand=1,fill=BOTH)])
+histB.place(bordermode=OUTSIDE,x=438,y=370)
+
+#### SHUTDOWN ####
+shutdownB = Button (main_frame, image=sd_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=window.destroy) 
+shutdownB.place(bordermode=OUTSIDE,x=650,y=370)
+
+###### BUTTONS FOR HISTORY UI #######
+###### BUTTON IMAGES LOAD #######
+bk = ImageTk.PhotoImage(Image.open("back.png"))
+nx = ImageTk.PhotoImage(Image.open("next.png"))
+pv = ImageTk.PhotoImage(Image.open("prev.png"))
+
+#### BACK ####
+backB = Button (hist_frame, image=bk, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack(expand=1,fill=BOTH),hist_frame.pack_forget()])
+backB.place(bordermode=OUTSIDE,x=25,y=20)
+
+#### NEXT ####
+nextB = Button (hist_frame, image=nx, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack(expand=1,fill=BOTH),hist_frame.pack_forget()])
+nextB.place(bordermode=OUTSIDE,x=465,y=380)
+
+#### PREVIOUS ####
+prevB = Button (hist_frame, image=pv, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack(expand=1,fill=BOTH),hist_frame.pack_forget()])
+prevB.place(bordermode=OUTSIDE,x=200,y=380)
+
+is_wifi()
+window.mainloop() #Start
+
+
+
     
-    ####### MAIN UI BG through Pillow PIL ########
-    main_bg = Canvas(main_frame, bg="#e3e3e3", height=480, width=848) 
-    main_bg_image = ImageTk.PhotoImage(Image.open("emptybg.png")) # BG through Pillow PIL
-    main_label = Label(main_frame, image=main_bg_image) 
-    main_label.place(x=0, y=0, relwidth=1, relheight=1) 
-    main_bg.pack()
-
-    ###### MAIN UI LABELS ###########
-    TTF = "₱"+str(Total_Fare)
-    main_totalfare = Label(main_frame, width="7", bd=0, bg="#e3e3e3", fg="#00ad31", font=("ArialUnicodeMS",55), text=TTF)
-    main_totalfare.place(x=70,y=32)
-
-    main_totalpass = Label(main_frame, width="7", bd=0, bg="#e3e3e3", fg="#00ad31", font=("ArialUnicodeMS",55), text=Total_Passenger)
-    main_totalpass.place(x=70,y=282)
-    
-    main_drivername = Label(main_frame, anchor="sw", width="25", bd=0, bg="#e3e3e3", fg="#000000", font=("ArialUnicodeMS",15), text=Driver_Name)
-    main_drivername.place(x=10,y=445) 
-    
-    nowifi_image = ImageTk.PhotoImage(Image.open("nowifi.png"))
-    nowifi_label = Label(main_frame, image=nowifi_image, bd=0, bg="#e3e3e3") 
-    #nowifi_label.place(x=10,y=10)
-
-    wifi_image = ImageTk.PhotoImage(Image.open("wifistatus.png"))
-    wifi_label = Label(main_frame, image=wifi_image, bd=0, bg="#e3e3e3") 
-    #wifi_label.place(x=10,y=10)
-
-    
-
-    ####### HISTORY UI BG through Pillow PIL ########
-    hist_bg = Canvas(hist_frame, bg="#e3e3e3", height=480, width=848) 
-    hist_bg_image = ImageTk.PhotoImage(Image.open("hist_screen.png")) # BG through Pillow PIL
-    hist_label = Label(hist_frame, image=hist_bg_image) 
-    hist_label.place(x=0, y=0, relwidth=1, relheight=1) 
-    hist_bg.pack()
-
-    ###### BUTTONS FOR MAIN UI #######
-    ###### BUTTON IMAGES LOAD #######
-    sd_image = ImageTk.PhotoImage(Image.open("sd.png"))
-    sy_image = ImageTk.PhotoImage(Image.open("sync.png"))
-    showhide_image = ImageTk.PhotoImage(Image.open("showhide.png"))
-    hist_image = ImageTk.PhotoImage(Image.open("hist.png"))
-
-    #### SYNC ####
-    syB = Button (main_frame, image=sy_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: sync(wifi_label,nowifi_label))
-    syB.place(bordermode=OUTSIDE,x=438,y=258)
-
-    #### SHOW/HIDE ####
-    showhideB = Button (main_frame, image=showhide_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: showhide(main_totalfare,main_totalpass))
-    showhideB.place(bordermode=OUTSIDE,x=650,y=258)
-
-    #### HISTORY ####
-    histB = Button (main_frame, image=hist_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack_forget(),hist_frame.pack(expand=1,fill=BOTH)])
-    histB.place(bordermode=OUTSIDE,x=438,y=370)
-
-    #### SHUTDOWN ####
-    shutdownB = Button (main_frame, image=sd_image, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=window.destroy) 
-    shutdownB.place(bordermode=OUTSIDE,x=650,y=370)
-
-    ###### BUTTONS FOR HISTORY UI #######
-    ###### BUTTON IMAGES LOAD #######
-    bk = ImageTk.PhotoImage(Image.open("back.png"))
-    nx = ImageTk.PhotoImage(Image.open("next.png"))
-    pv = ImageTk.PhotoImage(Image.open("prev.png"))
-
-    #### BACK ####
-    backB = Button (hist_frame, image=bk, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack(expand=1,fill=BOTH),hist_frame.pack_forget()])
-    backB.place(bordermode=OUTSIDE,x=25,y=20)
-
-    #### NEXT ####
-    nextB = Button (hist_frame, image=nx, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack(expand=1,fill=BOTH),hist_frame.pack_forget()])
-    nextB.place(bordermode=OUTSIDE,x=465,y=380)
-
-    #### PREVIOUS ####
-    prevB = Button (hist_frame, image=pv, width=182, height=74, highlightthickness=0, bd=0, bg="#e3e3e3", activebackground="#e3e3e3", command=lambda: [main_frame.pack(expand=1,fill=BOTH),hist_frame.pack_forget()])
-    prevB.place(bordermode=OUTSIDE,x=200,y=380)
-    
-    is_wifi()
-    window.mainloop() #Start
-    
-
-if __name__ == "__main__":
-    ##### WINDOW #####
-    window = Tk()
-    window.geometry("848x480") #Size for Window
-    # window.overrideredirect(1) #Remove window border
-    window.resizable(False,False) #Prevent resize windows
-    mainUI(window)
--- test.py
+++ test.py
@@ -1,58 +1,22 @@
-import tkinter as tk
+import socket
+import tkinter
 
-class Page(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-    def show(self):
-        self.lift()
+top = tkinter.Tk()
+top.title("Network Checker")
+top.configure(background="#006666")
 
-class Page1(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 1")
-       label.pack(side="top", fill="both", expand=True)
+l=tkinter.Label(top,text='Checking ...')
+l.pack()
 
-class Page2(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 2")
-       label.pack(side="top", fill="both", expand=True)
+def is_connected():
+    try:
+        socket.create_connection(("www.google.com", 80)) # better to set timeout as well
+        state = "Online"
+    except OSError:
+        state = "Offline"
+    l.config(text=state)
+    print(state)
+    top.after(1000, is_connected) # do checking again one second later
 
-class Page3(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 3")
-       label.pack(side="top", fill="both", expand=True)
-
-class MainView(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-        p1 = Page1(self)
-        p2 = Page2(self)
-        p3 = Page3(self)
-
-        buttonframe = tk.Frame(self)
-        container = tk.Frame(self)
-        buttonframe.pack(side="top", fill="x", expand=False)
-        container.pack(side="top", fill="both", expand=True)
-
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-
-        b1 = tk.Button(buttonframe, text="Page 1", command=p1.lift)
-        b2 = tk.Button(buttonframe, text="Page 2", command=p2.lift)
-        b3 = tk.Button(buttonframe, text="Page 3", command=p3.lift)
-
-        b1.pack(side="left")
-        b2.pack(side="left")
-        b3.pack(side="left")
-
-        p1.show()
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    main = MainView(root)
-    main.pack(side="top", fill="both", expand=True)
-    root.wm_geometry("400x400")
-    root.mainloop()
+is_connected() # start the checking
+top.mainloop()
