b2d23ee9cbe7f5dcf6f408905efced784e4f96c5
Bryle Raspi <brylempat@gmail.com>
2020-03-01 07:15:18 +0000

    Changed temp_Driver to actual driver

--- mainUI.py
+++ mainUI.py
@@ -24,7 +24,6 @@ Driver_ID = ("",)
 
 #initialization for RFID readers
 shuttlePrice='5'
-temp_DRIVERID='13'
 RFID_reader1 = SimpleMFRC522()
 RFID_reader2 = raspiRFID2.SimpleMFRC522a()
 buzzer1=31
@@ -95,72 +94,76 @@ def refresh():
         wifi_label.config(image=replace)
         wifi_label.image=replace
 
-        # #GREY RFID
-        # rfid_uid,rfid_idnum = RFID_reader1.read_no_block()
-        # global grey_counter
-        # print(rfid_idnum)
-        
-        # if(rfid_idnum!=None and grey_flag==1):
-        #     grey_counter = 0
-        
-        # if(grey_counter==40 and grey_flag==1):
-        #     print(grey_counter)
-        #     grey_counter = 0
-        #     grey_recent.config(text="",anchor="w")
-        # elif(grey_counter<40 and grey_flag==1):
-        #     grey_counter = grey_counter + 1
-        #     grey_recent.config(text=rfid_idnum,anchor="w")
-        
-        # # call blocking accounts func
-        # temporaryBlockFunc()
+        # readerr = SimpleMFRC522()
+        # uidd = readerr.read_no_block()
+        # print(uidd)
 
-        # # RFID READER
-        # rfid_uid, text = RFID_reader1.read_no_block()
-        # print("RFID1 UID="+str(rfid_uid))
-        # rfid_idNum=raspiRFID.checkUID(rfid_uid)
-        # print("IDNUM="+str(rfid_idNum))
+        #GREY RFID
+        rfid_uid,rfid_idnum = RFID_reader1.read_no_block()
+        global grey_counter
+        print(rfid_idnum)
         
-        # if(rfid_idNum!=None):
-        #     if(not findIfBlocked(rfid_idNum[0])):
-        #         if(rfid_idNum[1]==1):
-        #             transactionRecord= [(str(rfid_uid),str(datetime.datetime.now()),str(rfid_idNum[0]),int(shuttlePrice),str(temp_DRIVERID))]
-        #             raspiRFID.inputTransactiontoDB(transactionRecord)
-        #             raspiRFID.buzzSuccessful(buzzer1)
-        #             #add to blocked list
-        #             blockedAccounts.append([rfid_idNum[0],time.time()])
-        #             main_recent.config(text=rfid_idNum[0],anchor="w")
-        #         else:
-        #             raspiRFID.buzzNoBalance(buzzer1)
-        #             pass
-        #     else:
-        #         print("Blocked for 60 sec")
-        # else:
-        #     print('UID not in database')
-        #     #raspiRFID.buzzNotInDB(buzzer1)
+        if(rfid_idnum!=None and grey_flag==1):
+            grey_counter = 0
+        
+        if(grey_counter==40 and grey_flag==1):
+            print(grey_counter)
+            grey_counter = 0
+            grey_recent.config(text="",anchor="w")
+        elif(grey_counter<40 and grey_flag==1):
+            grey_counter = grey_counter + 1
+            grey_recent.config(text=rfid_idnum,anchor="w")
+        
+        # call blocking accounts func
+        temporaryBlockFunc()
 
-        # #SECOND RFID READER
-        # rfid_uid2, text2 = RFID_reader2.read_no_block()
-        # print("RFID2 UID="+str(rfid_uid2))
-        # rfid_idNum2=raspiRFID.checkUID(rfid_uid2)
-        # print('IDNUM2='+str(rfid_idNum2))
+        # RFID READER
+        rfid_uid, text = RFID_reader1.read_no_block()
+        print("RFID1 UID="+str(rfid_uid))
+        rfid_idNum=raspiRFID.checkUID(rfid_uid)
+        print("IDNUM="+str(rfid_idNum))
         
-        # if(rfid_idNum2!=None):
-        #     if(not findIfBlocked(rfid_idNum2[0])):
-        #         if(rfid_idNum2[1]==1):
-        #             transactionRecord= [(str(rfid_uid2),str(datetime.datetime.now()),str(rfid_idNum2[0]),int(shuttlePrice),str(temp_DRIVERID))]
-        #             raspiRFID.inputTransactiontoDB(transactionRecord)
-        #             raspiRFID.buzzSuccessful(buzzer2)
-        #             #add to blocked list
-        #             blockedAccounts.append([rfid_idNum2[0],time.time()])
-        #             main_recent.config(text=rfid_idNum2[0],anchor="w")
-        #         else:
-        #             raspiRFID.buzzNoBalance(buzzer2)
-        #             pass
-        #     else:
-        #         print("Blocked for 60 sec")
-        # else:
-        #         print('UID not in database')
-        #         # raspiRFID.buzzNotInDB(buzzer2)
+        if(rfid_idNum!=None):
+            if(not findIfBlocked(rfid_idNum[0])):
+                if(rfid_idNum[1]==1):
+                    transactionRecord= [(str(rfid_uid),str(datetime.datetime.now()),str(rfid_idNum[0]),int(shuttlePrice),Driver_ID[0])]
+                    raspiRFID.inputTransactiontoDB(transactionRecord)
+                    raspiRFID.buzzSuccessful(buzzer1)
+                    #add to blocked list
+                    blockedAccounts.append([rfid_idNum[0],time.time()])
+                    main_recent.config(text=rfid_idNum[0],anchor="w")
+                else:
+                    raspiRFID.buzzNoBalance(buzzer1)
+                    pass
+            else:
+                print("Blocked for 60 sec")
+        else:
+            print('UID not in database')
+            #raspiRFID.buzzNotInDB(buzzer1)
+
+        #SECOND RFID READER
+        rfid_uid2, text2 = RFID_reader2.read_no_block()
+        print("RFID2 UID="+str(rfid_uid2))
+        rfid_idNum2=raspiRFID.checkUID(rfid_uid2)
+        print('IDNUM2='+str(rfid_idNum2))
+        
+        if(rfid_idNum2!=None):
+            if(not findIfBlocked(rfid_idNum2[0])):
+                if(rfid_idNum2[1]==1):
+                    transactionRecord= [(str(rfid_uid2),str(datetime.datetime.now()),str(rfid_idNum2[0]),int(shuttlePrice),Driver_ID[0])]
+                    raspiRFID.inputTransactiontoDB(transactionRecord)
+                    raspiRFID.buzzSuccessful(buzzer2)
+                    #add to blocked list
+                    blockedAccounts.append([rfid_idNum2[0],time.time()])
+                    main_recent.config(text=rfid_idNum2[0],anchor="w")
+                else:
+                    raspiRFID.buzzNoBalance(buzzer2)
+                    pass
+            else:
+                print("Blocked for 60 sec")
+        else:
+                print('UID not in database')
+                # raspiRFID.buzzNotInDB(buzzer2)
 
     window.after(300, refresh)
 
